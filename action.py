ac20aee99e40e9e74c4827e04e8d1e46c034bdfc
pwn xyzz <pwnxyzz@pwndeMacBook-Pro.local>
2020-08-15 10:50:43 +0800

    new

--- action.py
+++ action.py
@@ -14,7 +14,7 @@ class getinfo:
         self.DOCKER_USERNAME = env.get('DOCKER_USERNAME')
         self.DOCKER_PASSWORD = env.get('DOCKER_PASSWORD')
         self.DOCKER_IMAGE_NAME = env.get('DOCKER_IMAGE_NAME')
-        self.DOCKER_IMAGE_TAG = env.get('DOCKER_IMAGE_NAME',time.strftime("%Y%m%d%H%M%S", time.localtime()))
+        self.DOCKER_IMAGE_TAG = env.get('DOCKER_IMAGE_TAG',time.strftime("%Y%m%d%H%M%S", time.localtime()))
         self.Push_latest = env.get('Push_latest',True)
         self.Only_Push_latest = env.get('Only_Push_latest',False)
         self.Dockerfile = env.get('Dockerfile','./Dockerfile')
@@ -26,7 +26,7 @@ os.system('echo {DOCKER_PASSWORD} | docker login --username {DOCKER_USERNAME} --
 
 build_name = '{DOCKER_IMAGE_NAME}:{DOCKER_IMAGE_TAG}'.format(DOCKER_IMAGE_NAME=info.DOCKER_IMAGE_NAME,DOCKER_IMAGE_TAG=info.DOCKER_IMAGE_TAG)
 latest_name = '{DOCKER_IMAGE_NAME}:latest'.format(DOCKER_IMAGE_NAME=info.DOCKER_IMAGE_NAME)
-if(info.Only_Push_latest==True or info.DOCKER_IMAGE_TAG=='latest'):
+if(info.Only_Push_latest!=False or info.DOCKER_IMAGE_TAG=='latest'):
     build_name = '{DOCKER_IMAGE_NAME}:latest'.format(DOCKER_IMAGE_NAME=info.DOCKER_IMAGE_NAME)
 
 os.system('docker build . --file {Dockerfile} --tag {build_name}'.format(Dockerfile=info.Dockerfile,build_name=build_name))
